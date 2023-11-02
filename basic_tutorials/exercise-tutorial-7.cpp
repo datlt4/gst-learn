75b9d59be65de6a91d789baa558016a01647ecfc
LuongTanDat <luongtandat.m0145@gmail.com>
2023-11-02 08:51:35 +0700

    feat: update exercise tutorial 7 add filesink

--- basic_tutorials/exercise-tutorial-7.cpp
+++ basic_tutorials/exercise-tutorial-7.cpp
@@ -6,55 +6,53 @@ typedef struct _CustomData
 {
     GstElement *pipeline;
     GstElement *source;
-    GstElement *tee;
-    // GstElement *audio_queue;
-    GstElement *audio_convert;
-    GstElement *audio_resample;
-    GstElement *audio_sink;
-    GstElement *filtered_video_queue;
-    GstElement *filtered_video_convert1;
-    GstElement *filtered_video_filter;
-    GstElement *filtered_video_convert2;
-    GstElement *filtered_video_sink;
-    GstElement *origin_video_queue;
-    GstElement *origin_video_convert;
-    GstElement *origin_video_sink;
+
+    GstElement *audio_convert, *audio_resample;
+    GstElement *tee_audio;
+    GstElement *audio_queue, *audio_sink;
+    GstElement *wavescope_queue, *wavescope, *wavescope_convert, *wavescope_sink;
+    GstElement *file_queue, *file_wavenc, *filesink;
+
+    GstElement *tee_video;
+    GstElement *filter_video_queue, *filter_video_convert1, *filter_video_filter, *filter_video_convert2,
+        *filter_video_sink;
+    GstElement *origin_video_queue, *origin_video_convert, *origin_video_sink;
 
     _CustomData()
-        : pipeline{nullptr}, source{nullptr}, tee{nullptr},
-          // audio_queue{nullptr},
-          audio_convert{nullptr}, audio_resample{nullptr}, audio_sink{nullptr}, filtered_video_queue{nullptr},
-          filtered_video_convert1{nullptr}, filtered_video_filter{nullptr}, filtered_video_convert2{nullptr},
-          filtered_video_sink{nullptr}, origin_video_queue{nullptr}, origin_video_convert{nullptr},
-          origin_video_sink{nullptr}
+        : pipeline{nullptr}, source{nullptr}, audio_convert{nullptr}, audio_resample{nullptr}, tee_audio{nullptr},
+          audio_queue{nullptr}, audio_sink{nullptr}, wavescope_queue{nullptr}, wavescope{nullptr},
+          wavescope_convert{nullptr}, wavescope_sink{nullptr}, file_queue{nullptr}, file_wavenc{nullptr},
+          filesink{nullptr}, tee_video{nullptr}, filter_video_queue{nullptr}, filter_video_convert1{nullptr},
+          filter_video_filter{nullptr}, filter_video_convert2{nullptr}, filter_video_sink{nullptr},
+          origin_video_queue{nullptr}, origin_video_convert{nullptr}, origin_video_sink{nullptr}
     {
     }
 
     gboolean checkValid(void)
     {
-        return (gboolean)(pipeline && source && tee &&
-                          // audio_queue &&
-                          audio_convert && audio_resample && audio_sink && filtered_video_queue &&
-                          filtered_video_convert1 && filtered_video_filter && filtered_video_convert2 &&
-                          filtered_video_sink && origin_video_queue && origin_video_convert && origin_video_sink);
+        return (gboolean)(pipeline && source && audio_convert && audio_resample && tee_audio && audio_queue &&
+                          audio_sink && wavescope_queue && wavescope && wavescope_convert && wavescope_sink &&
+                          file_queue && file_wavenc && filesink && tee_video && filter_video_queue &&
+                          filter_video_convert1 && filter_video_filter && filter_video_convert2 && filter_video_sink &&
+                          origin_video_queue && origin_video_convert && origin_video_sink);
     }
 
-    GstStateChangeReturn changeStatePlaying()
+    GstStateChangeReturn changeStatePlaying(void)
     {
         return gst_element_set_state(pipeline, GST_STATE_PLAYING);
     }
 
-    GstStateChangeReturn changeStateNull()
+    GstStateChangeReturn changeStateNull(void)
     {
         return gst_element_set_state(pipeline, GST_STATE_NULL);
     }
 
-    GstStateChangeReturn changeStateReady()
+    GstStateChangeReturn changeStateReady(void)
     {
         return gst_element_set_state(pipeline, GST_STATE_READY);
     }
 
-    GstStateChangeReturn changeStatePaused()
+    GstStateChangeReturn changeStatePaused(void)
     {
         return gst_element_set_state(pipeline, GST_STATE_PAUSED);
     }
@@ -77,37 +75,39 @@ int main(int argc, char **argv)
     GstStateChangeReturn ret;
     gboolean terminate = FALSE;
 
-    GstPad *tee_filtered_video_pad, *tee_origin_video_pad;
-    GstPad *queue_filtered_video_pad, *queue_origin_video_pad;
+    GstPad *tee_audio_pad, *queue_audio_pad;
+    GstPad *tee_wavescope_pad, *queue_wavescope_pad;
+    GstPad *tee_file_pad, *queue_file_pad;
+    GstPad *tee_filter_video_pad, *queue_filter_video_pad;
+    GstPad *tee_origin_video_pad, *queue_origin_video_pad;
 
     /* Initialize GStreamer */
     gst_init(&argc, &argv);
 
     /* Create the elements */
     data.source = gst_element_factory_make("uridecodebin", "source");
-    data.tee = gst_element_factory_make("tee", "tee");
 
-    // data.audio_queue = gst_element_factory_make("queue", "audio_queue");
     data.audio_convert = gst_element_factory_make("audioconvert", "audio_convert");
     data.audio_resample = gst_element_factory_make("audioresample", "audio_resample");
+    data.tee_audio = gst_element_factory_make("tee", "tee_audio");
+    data.audio_queue = gst_element_factory_make("queue", "audio_queue");
     data.audio_sink = gst_element_factory_make("autoaudiosink", "audio_sink");
+    data.wavescope_queue = gst_element_factory_make("queue", "wavescope_queue");
+    data.wavescope = gst_element_factory_make("wavescope", "wavescope");
+    data.wavescope_convert = gst_element_factory_make("videoconvert", "wavescope_convert");
+    data.wavescope_sink = gst_element_factory_make("autovideosink", "wavescope_sink");
+    data.file_queue = gst_element_factory_make("queue", "file_queue");
+    data.file_wavenc = gst_element_factory_make("wavenc", "file_wavenc");
+    data.filesink = gst_element_factory_make("filesink", "filesink");
 
-    data.filtered_video_queue = gst_element_factory_make("queue", "filtered_video_queue");
-    data.filtered_video_convert1 = gst_element_factory_make("videoconvert", "filtered_video_convert1");
-    data.filtered_video_filter = gst_element_factory_make("agingtv", "filtered_video_filter");
-    // data.filtered_video_filter = gst_element_factory_make("dicetv", "filtered_video_filter");
-    // data.filtered_video_filter = gst_element_factory_make("edgetv", "filtered_video_filter");
-    // data.filtered_video_filter = gst_element_factory_make("optv", "filtered_video_filter");
-    // data.filtered_video_filter = gst_element_factory_make("quarktv", "filtered_video_filter");
-    // data.filtered_video_filter = gst_element_factory_make("radioactv", "filtered_video_filter");
-    // data.filtered_video_filter = gst_element_factory_make("revtv", "filtered_video_filter");
-    // data.filtered_video_filter = gst_element_factory_make("rippletv", "filtered_video_filter");
-    // data.filtered_video_filter = gst_element_factory_make("shagadelictv", "filtered_video_filter");
-    // data.filtered_video_filter = gst_element_factory_make("streaktv", "filtered_video_filter");
-    // data.filtered_video_filter = gst_element_factory_make("vertigotv", "filtered_video_filter");
-    // data.filtered_video_filter = gst_element_factory_make("warptv", "filtered_video_filter");
-    data.filtered_video_convert2 = gst_element_factory_make("videoconvert", "filtered_video_convert2");
-    data.filtered_video_sink = gst_element_factory_make("autovideosink", "filtered_video_sink");
+    data.tee_video = gst_element_factory_make("tee", "tee_video");
+    data.filter_video_queue = gst_element_factory_make("queue", "filter_video_queue");
+    data.filter_video_convert1 = gst_element_factory_make("videoconvert", "filter_video_convert1");
+    data.filter_video_filter = gst_element_factory_make("agingtv", "filter_video_filter");
+    //"agingtv" | "dicetv" | "edgetv" | "optv" | "quarktv" | "radioactv" | "revtv" | "rippletv" | "shagadelictv" |
+    //"streaktv" | "vertigotv" | "warptv";
+    data.filter_video_convert2 = gst_element_factory_make("videoconvert", "filter_video_convert2");
+    data.filter_video_sink = gst_element_factory_make("autovideosink", "filter_video_sink");
 
     data.origin_video_queue = gst_element_factory_make("queue", "origin_video_queue");
     data.origin_video_convert = gst_element_factory_make("videoconvert", "origin_video_convert");
@@ -122,18 +122,21 @@ int main(int argc, char **argv)
         return -1;
     }
 
-    /* Set the URI to play */
-    g_object_set(data.source, "uri", "https://gstreamer.freedesktop.org/data/media/sintel_trailer-480p.webm", NULL);
-
     /* Build the pipeline. Note that we are NOT linking the source at this point. We will do it later. */
-    gst_bin_add_many(GST_BIN(data.pipeline), data.source, data.tee, data.audio_convert, data.audio_resample,
-                     data.audio_sink, data.filtered_video_queue, data.filtered_video_convert1,
-                     data.filtered_video_filter, data.filtered_video_convert2, data.filtered_video_sink,
-                     data.origin_video_queue, data.origin_video_convert, data.origin_video_sink, NULL);
+    gst_bin_add_many(GST_BIN(data.pipeline), data.source, data.audio_convert, data.audio_resample, data.tee_audio,
+                     data.audio_queue, data.audio_sink, data.wavescope_queue, data.wavescope, data.wavescope_convert,
+                     data.wavescope_sink, data.file_queue, data.file_wavenc, data.filesink, data.tee_video,
+                     data.filter_video_queue, data.filter_video_convert1, data.filter_video_filter,
+                     data.filter_video_convert2, data.filter_video_sink, data.origin_video_queue,
+                     data.origin_video_convert, data.origin_video_sink, NULL);
 
-    if (gst_element_link_many(data.audio_convert, data.audio_resample, data.audio_sink, NULL) != TRUE ||
-        gst_element_link_many(data.filtered_video_queue, data.filtered_video_convert1, data.filtered_video_filter,
-                              data.filtered_video_convert2, data.filtered_video_sink, NULL) != TRUE ||
+    if (gst_element_link_many(data.audio_convert, data.audio_resample, data.tee_audio, NULL) != TRUE ||
+        gst_element_link_many(data.audio_queue, data.audio_sink, NULL) != TRUE ||
+        gst_element_link_many(data.wavescope_queue, data.wavescope, data.wavescope_convert, data.wavescope_sink,
+                              NULL) != TRUE ||
+        gst_element_link_many(data.file_queue, data.file_wavenc, data.filesink, NULL) != TRUE ||
+        gst_element_link_many(data.filter_video_queue, data.filter_video_convert1, data.filter_video_filter,
+                              data.filter_video_convert2, data.filter_video_sink, NULL) != TRUE ||
         gst_element_link_many(data.origin_video_queue, data.origin_video_convert, data.origin_video_sink, NULL) != TRUE)
     {
         g_printerr("Elements could not be linked.\n");
@@ -142,31 +145,59 @@ int main(int argc, char **argv)
     }
 
     /* Manually link the Tee, which has "Request" pads */
-    tee_filtered_video_pad = gst_element_get_request_pad(data.tee, "src_%u");
-    g_print("Obtained request pad %s for filtered_video branch.\n", gst_pad_get_name(tee_filtered_video_pad));
-    queue_filtered_video_pad = gst_element_get_static_pad(data.filtered_video_queue, "sink");
+    tee_audio_pad = gst_element_get_request_pad(data.tee_audio, "src_%u");
+    queue_audio_pad = gst_element_get_static_pad(data.audio_queue, "sink");
+    g_print("Obtained request pad %s for audio branch.\n", gst_pad_get_name(tee_audio_pad));
 
-    tee_origin_video_pad = gst_element_get_request_pad(data.tee, "src_%u");
-    g_print("Obtained request pad %s for origin_video branch.\n", gst_pad_get_name(tee_origin_video_pad));
+    tee_wavescope_pad = gst_element_get_request_pad(data.tee_audio, "src_%u");
+    queue_wavescope_pad = gst_element_get_static_pad(data.wavescope_queue, "sink");
+    g_print("Obtained request pad %s for wavescope branch.\n", gst_pad_get_name(tee_wavescope_pad));
+
+    tee_file_pad = gst_element_get_request_pad(data.tee_audio, "src_%u");
+    queue_file_pad = gst_element_get_static_pad(data.file_queue, "sink");
+    g_print("Obtained request pad %s for wavescope branch.\n", gst_pad_get_name(tee_file_pad));
+
+    tee_filter_video_pad = gst_element_get_request_pad(data.tee_video, "src_%u");
+    queue_filter_video_pad = gst_element_get_static_pad(data.filter_video_queue, "sink");
+    g_print("Obtained request pad %s for filter_video branch.\n", gst_pad_get_name(tee_filter_video_pad));
+
+    tee_origin_video_pad = gst_element_get_request_pad(data.tee_video, "src_%u");
     queue_origin_video_pad = gst_element_get_static_pad(data.origin_video_queue, "sink");
+    g_print("Obtained request pad %s for origin_video branch.\n", gst_pad_get_name(tee_origin_video_pad));
 
-    GstPadLinkReturn ra = gst_pad_link(tee_filtered_video_pad, queue_filtered_video_pad);
+    GstPadLinkReturn ra = gst_pad_link(tee_audio_pad, queue_audio_pad);
+    GstPadLinkReturn rw = gst_pad_link(tee_wavescope_pad, queue_wavescope_pad);
+    GstPadLinkReturn rs = gst_pad_link(tee_file_pad, queue_file_pad);
+    GstPadLinkReturn rf = gst_pad_link(tee_filter_video_pad, queue_filter_video_pad);
     GstPadLinkReturn rv = gst_pad_link(tee_origin_video_pad, queue_origin_video_pad);
 
-    std::cout << "ra: " << ra << "  rv: " << rv << std::endl;
+    std::cout << "ra: " << ra << "  rw: " << rw << "  rs: " << rs << "  rf: " << rf << "  rv: " << rv << std::endl;
 
-    if (ra != GST_PAD_LINK_OK || rv != GST_PAD_LINK_OK)
+    if (ra != GST_PAD_LINK_OK || rw != GST_PAD_LINK_OK || rs != GST_PAD_LINK_OK || rf != GST_PAD_LINK_OK ||
+        rv != GST_PAD_LINK_OK)
     {
         g_printerr("Tee could not be linked.\n");
         gst_object_unref(data.pipeline);
         return -1;
     }
 
-    gst_object_unref(queue_filtered_video_pad);
+    gst_object_unref(queue_audio_pad);
+    gst_object_unref(queue_wavescope_pad);
+    gst_object_unref(queue_file_pad);
+    gst_object_unref(queue_filter_video_pad);
     gst_object_unref(queue_origin_video_pad);
 
     /* Connect to the pad-added signal */
     g_signal_connect(data.source, "pad-added", G_CALLBACK(pad_added_handler), &data);
+
+    /* Set the URI to play */
+    g_object_set(data.source, "uri", "https://gstreamer.freedesktop.org/data/media/sintel_trailer-480p.webm", NULL);
+
+    /* Set the location to save audio file */
+    g_object_set(data.filesink, "location", "test.wav", NULL);
+
+    /* Set the audio visualize style */
+    g_object_set(data.wavescope, "shader", 0, "style", 1, NULL);
 
     /* Start playing */
     ret = gst_element_set_state(data.pipeline, GST_STATE_PLAYING);
@@ -229,9 +260,15 @@ int main(int argc, char **argv)
     } while (!terminate);
 
     /* Release the request pads from the Tee, and unref them */
-    gst_element_release_request_pad(data.tee, tee_filtered_video_pad);
-    gst_element_release_request_pad(data.tee, tee_origin_video_pad);
-    gst_object_unref(tee_filtered_video_pad);
+    gst_element_release_request_pad(data.tee_audio, tee_audio_pad);
+    gst_element_release_request_pad(data.tee_audio, tee_wavescope_pad);
+    gst_element_release_request_pad(data.tee_audio, tee_file_pad);
+    gst_element_release_request_pad(data.tee_video, tee_filter_video_pad);
+    gst_element_release_request_pad(data.tee_video, tee_origin_video_pad);
+    gst_object_unref(tee_audio_pad);
+    gst_object_unref(tee_wavescope_pad);
+    gst_object_unref(tee_file_pad);
+    gst_object_unref(tee_filter_video_pad);
     gst_object_unref(tee_origin_video_pad);
 
     /* Free resources */
@@ -264,7 +301,7 @@ static void pad_added_handler(GstElement *src, GstPad *new_pad, CustomData *data
     }
     else if (g_str_has_prefix(new_pad_type, "video/x-raw"))
     {
-        sink_pad = gst_element_get_static_pad(data->tee, "sink");
+        sink_pad = gst_element_get_static_pad(data->tee_video, "sink");
     }
     else
     {
